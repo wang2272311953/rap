be5523d8a5685823694a5f56367a579aca7a39ba
wc <2272311953@qq.com>
2019-09-09 09:37:17 +0800

    提交了

---
+++ src/main/java/com/wangcheng/common/Utils.java
@@ -0,0 +1,26 @@
+package com.wangcheng.common;
+
+public class Utils {
+	public static String toHtml(String text){
+		String replaceAll = text.replaceAll("\n", "<p></p>");
+		String all = replaceAll.replaceAll("\r", "<br>");
+		String replace = all.replaceAll("\n\r", "\n");
+		return replace;
+	}
+	public static boolean toPhone(String phone){
+		String f="^1(3|5|7|8|9)[0-9]\\d{8}$";
+		boolean matches = phone.matches(f);
+		if (matches) {
+			return true;
+		}
+		return false;
+	}
+	public static boolean isEmail(String email){
+		String f="^([a-zA-Z0-9]+)@([a-zA-Z0-9]+\\.)(com|cn)$";
+		boolean matches = email.matches(f);
+		if (matches) {
+			return true;
+		}
+		return false;
+	}
+}
